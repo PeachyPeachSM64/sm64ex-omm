2170267659f90a300a958afc1fc87b3c942d0e10
U-DESKTOP-F0TJMO2\Utilisateur <peachy.peach.sm64@gmail.com>
2023-02-25 23:04:07 +0100

    v7.5.1

--- omm_builder.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set mingw64exe="%appdata%\msys2\mingw64.exe"
-if exist %mingw64exe% (
-	%mingw64exe% bash omm_builder.sh
-) else (
-	echo mingw64.exe not found. Please run the batch script 'omm_builder_setup.bat' first.
-	pause
-)
